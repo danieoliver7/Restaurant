92da3d71b73746d08095eea2c98d2875d3d7273b
danieoliver7 <danieoliver7@gmail.com>
2023-11-21 20:24:38 -0300

    Method post in resume

--- src/main/java/com/example/cardapio/demo/controller/ResumeController.java
+++ src/main/java/com/example/cardapio/demo/controller/ResumeController.java
@@ -3,11 +3,18 @@ package com.example.cardapio.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cardapio.demo.dto.ResumeDto;
+import com.example.cardapio.demo.entities.Resume;
+import com.example.cardapio.demo.repositories.ResumeRepositorie;
 import com.example.cardapio.demo.service.ResumeService;
 
 @RestController
@@ -17,10 +24,21 @@ public class ResumeController {
     @Autowired
     private ResumeService resumeService;
 
+   
+
+
     @GetMapping
     public List<ResumeDto> findAll(){
         List<ResumeDto> res = resumeService.findall();
         return res;
     }
 
+    @PostMapping
+    public void save(@RequestBody Resume resume){
+        if(resume != null){
+        resumeService.create(resume);
+        }else{
+            ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/example/cardapio/demo/service/ResumeService.java
+++ src/main/java/com/example/cardapio/demo/service/ResumeService.java
@@ -3,8 +3,11 @@ package com.example.cardapio.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.cardapio.demo.repositories.ResumeRepositorie;
 
@@ -23,6 +26,11 @@ public class ResumeService {
         List<Resume> result = resumeRepositorie.findAll();
         List<ResumeDto> dto = result.stream().map(x -> new ResumeDto(x)).toList();
         return dto;
+    }
+
+    public ResponseEntity create(@RequestBody Resume resume){
+    resumeRepositorie.save(resume);
+    return ResponseEntity.status(HttpStatus.CREATED).body(resume);
     }
 
 }
